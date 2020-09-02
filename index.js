25d9a7e7670c5b4aa698df3b6d03be063be8ec01
HaraKeisuke <mtnnnnnnn@gmail.com>
2020-09-02 20:29:05 +0900

    画像をランダムに返すように

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const express = require("express");
-const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== "production";
-
-const server = express();
-
-server.get("/", (req, res) => {
-  return res.status(200).end();
-});
-
-server.listen(port, (err) => {
-  if (err) throw err;
-  console.log(`> Ready on http://localhost:${port}`);
-});
-
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const token = process.env.token;
@@ -22,23 +7,25 @@ const points = [...new Array(16)].map((_, i) => i + 1);
 client.on("ready", () => {
   console.log("ready...");
 });
-//Bot自身の発言を無視する呪い
+
 client.on("message", (message) => {
   if (message.author.bot) {
     return;
   }
-  //↓ここに後述のコードをコピペする↓
   if (message.content === "/random" || message.content === "/r") {
-    message.channel
-      .send("Your Point is...", {
-        file: "/" + Math.floor(Math.random(points.length)) + "jpg",
-      })
-      .catch(console.error);
+    message.channel.send("Your Point is...");
+
+    const min = 1;
+    const max = 17;
+    const point = Math.floor(Math.random() * (max + 1 - min)) + min;
+    const attachment = new Discord.MessageAttachment(
+      "https://storage.googleapis.com/fortnite-map-pointer/" + point + ".jpg"
+    );
+    message.channel.send(attachment);
     return;
   }
   if (message.content.match(/discord.gg/)) {
     message.delete(100);
   }
-  //↑ここに後述のコードをコピペする↑
 });
 client.login(token);
